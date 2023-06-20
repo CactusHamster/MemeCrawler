f741e80f8e811c30ae9891d8c9b056c5bc6f024b
Robin <66969441+CactusHamster@users.noreply.github.com>
2023-06-20 16:53:15 -0700

    ChunkHandler fix.

--- src/chunkhandler.ts
+++ src/chunkhandler.ts
@@ -13,10 +13,12 @@ export class ChunkHandler<T> {
     get chunkLength (): number { return this.#chunkLength }
     checkChunks () {
         if (this.#pool.length < this.#chunkLength) return;
-        for (let i = 0; i < this.#pool.length; i += this.#chunkLength) {
+        let maxi = this.#pool.length - (this.#pool.length % this.#chunkLength);
+        for (let i = 0; i < maxi; i += this.#chunkLength) {
             let chunk = this.#pool.slice(i, i + this.#chunkLength);
             this.#emitter.emit("chunk", chunk);
         }
+        this.#pool.splice(0, maxi);
     }
     addItems (items: T[]) {
         this.#pool.push(...items);
@@ -24,13 +26,17 @@ export class ChunkHandler<T> {
     }
     push(...items: T[]) {
         this.#pool.push(...items);
-        this.checkChunks;
+        this.checkChunks();
     }
     on (event: "chunk", handler: (chunk: T[]) => any) {
         this.#emitter.on(event, handler);
     }
     off (event: "chunk", handler: (chunk: T[]) => any) {
         this.#emitter.off(event, handler);
+    }
+    end () {
+        this.checkChunks();
+        this.#emitter.emit("chunk", this.#pool);
     }
     destroy () {
         this.#pool.length = 0;
